0ff2629294f3b6ae7bef47e120f67ffc600a1327
FlorentAzd <florent4real@yahoo.com>
2022-02-25 12:05:31 +0000

    Ajout de fichier
    Creation du fichier README.md et ajout du moteur "ejs"

---
+++ README.md
@@ -0,0 +1,13 @@
+# Source Code social network
+Create a social network with the mvc model
+
+## Language & Framework :
+1. Front-End
+    * HTML
+    * CSS
+2. Back-End
+    * NodeJS
+3. Database
+    * SQL (MySQL)
+*** 
+Powered by [FLorentAzd](https://github.com/FLorentAzd) (Started February 25th, 2022)
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-const express = require('express');
+let express = require('express');
+
+let app = express();
+
+let indexRouter = require('./routes/index')
+
+// Define path of static file
+app.use(express.static(__dirname+'/assets'))
+
+//Defined view  path
+app.set("views", __dirname+"/views");
+app.set('view engine', 'ejs');
+
+app.use("/", indexRouter)
+
+
+// Faire écouter notre server sur un port ouvert
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Server running on port http://localhost:8080")
+});
+
+module.exports = app
